efa288cbfa4f399d42d3f6a35d6b255f80593433
Mrblackhatduck <mrblackhatduck@gmail.com>
2024-09-18 14:55:47 +0300

    Feat: Adding a Vec3 and basic node structure

--- AABB.h
+++ AABB.h
@@ -1,10 +1,19 @@
 #ifndef AABB_H
 #define AABB_H
 
+#include <glm/glm.hpp>
+using vec3 = glm::vec3;
+using vec4 = glm::vec4;
 
 class AABB
 {
-
+    vec3 Min;
+    vec3 Max;
+    AABB(vec3&& min, vec3&& max)
+    {
+        Min = std::forward<vec3>(min);
+        Max = std::forward<vec3>(max);
+    }
     
 
 };
